5a4625fd4a3d84a135f9b816970dafac1faa59f3
Miguel Escobar Godoy <mescob11@ucsc.edu>
2025-05-19 23:33:52 -0700

    extra merge tool

---
+++ src/psctsimpipe/tools/SubmitFullDirCtapipeMergeSLURM.py
@@ -0,0 +1,153 @@
+import argparse
+import os
+
+from psctsimpipe.CtapipeMergeCommand import create_ctapipe_merge_command
+from psctsimpipe.SLURMScriptGen import create_ctapipe_slurm_script, submit_job
+from psctsimpipe.Helpers import find_files
+
+def main():
+    parser = argparse.ArgumentParser(
+        usage = """submit-all-ctapipe-merge-SLURM-run \\
+            --input-dir <input_dir> \\
+            --output-dir <output_dir> \\
+            --search-pattern '*dl1.h5' \\
+            -c <cfg> \\
+            [OPTIONS]
+            """,
+        description="""Submit ctapipe-merge job through SLURM 
+                    for all ctapipe h5 files in directory.
+                    For an input directory of the form
+                    {particle_type}_{ze}_{az}_run#_{telescope_name}-{height}-{night_type}-{NSB},
+                    all files within this directory ending in pattern will be merged using ctapipe-merge.
+
+                    The outputs (merged h5, log, provenance) will follow this naming convention:
+                    output_dir/{input_dir}.dl1.h5
+                    output_dir/{input_dir}.log
+                    output_dir/{input_dir}.provenance.log
+                    """,
+        epilog="""Example: \n 
+        submit-all-ctapipe-merge-SLURM-run  
+        --input-dir /gamma/h5/
+        --output-dir /gamma/h5/  
+        --search-pattern '*dl1.h5'
+        -c /path/to/ctapipe_config.yaml
+        """
+        )
+    # ctapipe-merge
+    parser.add_argument(
+        "--input-dir",
+        help="path to directory where all sim_telarray files live."
+    )
+    parser.add_argument(
+        "--output-dir",
+        help="""path for ctapipe-process outputs, 
+                commandline standrd output and error files,
+                and slurm scripts.
+            """
+    )
+    parser.add_argument(
+        "--search-pattern",
+        default="'*dl1.h5'",
+        help="Give a specific file pattern for matching files in input-dir"
+    )
+    parser.add_argument(
+        "-c",
+        "--ctapipe_cfg",
+        default=None,
+        help="path to ctapipe config file"
+    )
+    # SLURM options
+    parser.add_argument(
+        "--email", 
+        default="",
+        help="Email for job notifications"
+        )
+    parser.add_argument(
+        "--mem", 
+        default="8G", 
+        help="Memory per node (e.g., 1G, 10G, etc.)"
+        )
+    parser.add_argument(
+        "--nodes", 
+        default=1, 
+        type=int, 
+        help="Number of nodes requested"
+        )
+    parser.add_argument(
+        "--n-tasks", 
+        default=1, 
+        type=int, 
+        help="Number of task per CPU core"
+        )
+    parser.add_argument(
+        "--cpus-per-task", 
+        default=1, 
+        type=int, 
+        help="Number of CPU cores to use per task"
+        )
+    parser.add_argument(
+        "-t", 
+        "--t_exp", 
+        default="2:00:00", 
+        help="Time allocated before job expires (e.g., HH:MM:SS)"
+        )
+    parser.add_argument(
+        "--partition", 
+        default="128x24",
+        help="Partition/queue name"
+        )
+    parser.add_argument(
+        "--qos",
+        default="",
+        help="Required to target VERITAS/SCT HB node. Set it to g-veritas if this is the case."
+    )
+    parser.add_argument(
+        "--account",
+        default="",
+        help="Required to target VERITAS/SCT HB node. Set it to g-veritas if this is the case"
+    )
+    parser.add_argument(
+        "--mail-type", 
+        default="END,FAIL",
+        help="Type of email notification to receive"
+        )
+    args = parser.parse_args()
+
+    files_to_process = find_files(args.input_dir,
+                                  search_pattern=args.search_pattern)
+
+    print(f"Found {len(files_to_process)} files in {args.input_dir} matching pattern '{args.search_pattern}'.")
+    print("These files will be merged.")
+
+        
+    command = create_ctapipe_merge_command(
+        args.input_dir,
+        args.output_dir,
+        f"'{args.search_pattern}'",
+        args.ctapipe_cfg,
+    )
+        
+    base_filename = os.path.basename(os.path.normpath(args.input_dir))
+    job_name = f"{base_filename}.ctapipe-merge"
+
+    script_path = create_ctapipe_slurm_script(
+        job_name, 
+        command, 
+        args.email, 
+        args.output_dir, 
+        args.mem, 
+        args.nodes, 
+        args.n_tasks,
+        args.cpus_per_task,
+        args.t_exp,
+        args.partition,
+        args.qos,
+        args.account,
+        args.mail_type
+        )
+    
+    submit_job(script_path)
+    print(f"Job submitted! SLURM script: {script_path}")
+    
+if __name__ == "__main__":
+    main()
